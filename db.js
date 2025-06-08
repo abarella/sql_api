b9f864118d84f18fa7c31aa5b09a6550c404e91c
abarella <alberto@abjinfo.com.br>
2025-06-08 18:12:47 -0300

    FACADE GOF METHOD

--- db.js
+++ db.js
@@ -1,101 +1,90 @@
 const sql = require('mssql');
 
-const config ={
-    server: "TICOTECO",
-    database: "VendasPelicano",
-    user: "crsa",
-    password: "cr9537",
-    options: {
+class DatabaseFacade {
+  constructor() {
+    this.config = {
+      server: "TICOTECO",
+      database: "VendasPelicano",
+      user: "crsa",
+      password: "cr9537",
+      options: {
         trustedConnection: true,
         enableArithAbort: true,
         trustServerCertificate: true,
-    },
-};
+      },
+    };
+  }
 
-// Normal queries to db handled here
-async function executeQuery(query, values = [], paramNames = [], isStoredProcedure = true, outputParamName = null) {
+  async connect() {
+    return sql.connect(this.config);
+  }
+
+  async executeQuery(query, values = [], paramNames = [], isStoredProcedure = true, outputParamName = null) {
     try {
-      const pool = await sql.connect(config);
+      const pool = await this.connect();
       const request = pool.request();
-  
+
       if (values && paramNames) {
         for (let i = 0; i < values.length; i++) {
           request.input(paramNames[i], values[i]);
         }
       }
-  
-      // Handle output parameter
+
       if (outputParamName) {
         request.output(outputParamName, sql.Int);
       }
-      
-      // console.log("VALUES ", values);
-      // console.log("PARAM ", paramNames);
-      // console.log("QUERY " , query);
-      // console.log("REQUEST ", request.parameters);
+
       values.forEach((val, index) => {
         if (typeof val === 'undefined') {
-          console.error(`Undefined value found for ${paramNames[index]}`);
+          console.error(`Undefined value for param ${paramNames[index]}`);
         }
       });
-      
-      let result;
-      if (isStoredProcedure) {
-        result = await request.execute(query);
-      } else {
-        result = await request.batch(query);
-      }
-  
+
+      let result = isStoredProcedure
+        ? await request.execute(query)
+        : await request.batch(query);
+
       if (outputParamName) {
         result = { ...result, [outputParamName]: request.parameters[outputParamName].value };
       }
-  
+
       return result;
-    
     } catch (error) {
-      console.log(error);
+      console.error("Error executing query:", error);
       throw error;
     }
   }
-  
-  // Bulk queries handled here
-  async function executeTableValuedQuery(query, table, paramNames = [], isStoredProcedure = true, outputParamName = null) {
+
+  async executeTableValuedQuery(query, table, paramName, isStoredProcedure = true, outputParamName = null) {
     try {
-      const pool = await sql.connect(config);
+      const pool = await this.connect();
       const request = pool.request();
-  
-      // Setting the table-valued parameter
+
       if (table instanceof sql.Table) {
-        request.input(paramNames, table);
+        request.input(paramName, table);
       }
-  
-      // Handle output parameter
+
       if (outputParamName) {
         request.output(outputParamName, sql.Int);
       }
-  
-      let result;
-      if (isStoredProcedure) {
-        result = await request.execute(query);
-      } else {
-        result = await request.batch(query);
-      }
-  
+
+      let result = isStoredProcedure
+        ? await request.execute(query)
+        : await request.batch(query);
+
       if (outputParamName) {
         result = { ...result, [outputParamName]: request.parameters[outputParamName].value };
       }
-      
+
       return result;
     } catch (error) {
-      console.log(error);
+      console.error("Error executing table-valued query:", error);
       throw error;
     }
   }
-
-module.exports = {
-    connect: () => sql.connect(config),
-    sql,
-    executeQuery,
-    executeTableValuedQuery
 }
 
+module.exports = {
+  DatabaseFacade,
+  sql, // opcional: exportado para criação de tipos ou Table externamente
+};
